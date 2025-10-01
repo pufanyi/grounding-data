b00d81f8542c9b613291c96fe93a07fdbce19244
pufanyi <pufanyi@sensetime.com>
2025-10-01 05:55:32 +0000

    Refactor __init__.py to include model retrieval function and update __all__ exports

--- grounding/models/__init__.py
+++ grounding/models/__init__.py
@@ -1,12 +1,12 @@
 from .gpt_oss import GPTOSS
 from .model import Model
 
-
 __all__ = ["get_model"]
 
 __model_map__ = {
     "gpt-oss": GPTOSS,
 }
+
 
 def get_model(model_name: str, *args, **kwargs) -> Model:
     if model_name in __model_map__:
